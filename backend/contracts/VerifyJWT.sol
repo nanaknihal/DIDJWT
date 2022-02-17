7a8ebf4ca5b0cdf972fa8a6c6096fe533c6c8f38
nanaknihal <nanaknihal@gmail.com>
2022-02-17 01:04:44 -0700

    wow i havent commited in a long time

--- backend/contracts/VerifyJWT.sol
+++ backend/contracts/VerifyJWT.sol
@@ -13,10 +13,12 @@ contract VerifyJWT {
     mapping(bytes32 => JWTProof) public jwtProofs;
 
     // web2 server's RS256 public key, split into base, exponent, and modulus
-    uint256 b;
-    uint256 e;
-    uint256 m;
+    uint256 public b;
+    uint256 public e;
+    uint256 public m;
 
+    bytes32[] public pendingVerification; //unneeded later, just for testing purposes
+    bytes32[] public verifiedUsers;
     // function verify(uint256 base_length, uint256 exponent_length, uint256 modulus_length, bytes memory base, bytes memory exponent, bytes memory modulus){
     //   assembly {
     //   // call ecmul precompile
@@ -25,28 +27,37 @@ contract VerifyJWT {
     //   }
     // }
     // }
-
+    constructor(uint256 base, uint256 exponent, uint256 modulus){
+      b = base;
+      exponent = exponent;
+      modulus = modulus;
+    }
+    function testFunction(uint256 newE) public {
+      e = newE;
+    }
     // Why am i putting test functions here haha
-    function testAddressByteConversion(address a) public view returns (bool) {
+    function testAddressByteConversion(address a) public pure returns (bool) {
       return bytesToAddress(addressToBytes(a)) == a;
     }
 
     // https://ethereum.stackexchange.com/questions/8346/convert-address-to-string
-    function bytesToAddress(bytes memory b) private pure returns (address addr) {
+    function bytesToAddress(bytes memory b_) private pure returns (address addr) {
       assembly {
-        addr := mload(add(b,20))
+        addr := mload(add(b_,20))
       } 
     }
 
-    function bytes32ToAddress(bytes32 b) private pure returns (address addr) {
+    function bytes32ToAddress(bytes32 b_) private pure returns (address addr) {
       assembly {
-        addr := mload(add(b,20)) //shouldn't it be 0x20 or is that equivalent
+        addr := mload(add(b_,20)) //shouldn't it be 0x20 or is that equivalent
       } 
     }
 
-    function bytesToFirst32BytesAsBytes32Type(bytes memory b) private pure returns (bytes32 b32) {
+    function bytesToFirst32BytesAsBytes32Type(bytes memory input_) private view returns (bytes32 b_) {
       assembly {
-        b32 := mload(add(b,20)) //shouldn't it be 0x20 or is that equivalent
+        // there is probably an easier way to do this
+        let unshifted := mload(add(input_,32))
+        b_ := shr(96, unshifted)
       } 
     }
     
@@ -54,8 +65,8 @@ contract VerifyJWT {
       return abi.encodePacked(a);
     }
     
-    function bytes32ToBytes(bytes32 b) private pure returns (bytes memory){
-      return abi.encodePacked(b);
+    function bytes32ToBytes(bytes32 b_) private pure returns (bytes memory){
+      return abi.encodePacked(b_);
     }
     // function addressToBytes32(address a) public pure returns (bytes32) {
     //   return abi.encodePacked(a);
@@ -64,7 +75,10 @@ contract VerifyJWT {
     function stringToBytes(string memory s) public pure returns (bytes memory) {
       return abi.encodePacked(s);
     }
-    
+    // logging function to support bytes32, which hardhat doesn't support
+    function logBytes32(bytes32 b_) internal view {
+      console.logBytes(bytes32ToBytes(b_));
+    }
     // https://ethereum.stackexchange.com/questions/71565/verifying-modular-exponentiation-operation-in-etherum
     function modExp(uint256 _b, uint256 _e, uint256 _m) public returns (uint256 result) {
         assembly {
@@ -111,19 +125,40 @@ contract VerifyJWT {
     }
 
     function commitJWTProof(bytes32 jwtXORPubkey, bytes32 jwtHash) public {
-    jwtProofs[jwtXORPubkey] = JWTProof({
-      blockNumber: block.number, 
-      hashedJWT: jwtHash
-    });
-
-  }
-  function _checkJWTProof(address a, string memory jwt) private view returns (bool) {
+      console.logBytes32(jwtXORPubkey);
+      console.log("that was key");
+      jwtProofs[jwtXORPubkey] = JWTProof({
+        blockNumber: block.number, 
+        hashedJWT: jwtHash
+      });
+      pendingVerification.push(jwtXORPubkey);
+    }
+  // perhaps make private, but need it to be public to test
+  function checkJWTProof(address a, string memory jwt) public view returns (bool) {
     bytes32 bytes32Pubkey = bytesToFirst32BytesAsBytes32Type(addressToBytes(a));
+    console.log("address version:");
+    console.log(a);
+    console.logBytes(addressToBytes(a));
+    console.logBytes32(bytes32Pubkey);
+    console.log("^bytes32 version");
+    
     // check whether sender has already proved knowledge of the jwt in a previous block by XORing it with their public key and SHA2 of JWT. 
     // CANNOT use same encryption algorithm that jp.hashedJWT is stored with; that would cause an attack vector:
     // hash(JWT) would be known, so then XOR(public key, hash(JWT)) can be replaced with XOR(frontrunner pubkey, hash(JWT)) by a frontrunner
     bytes32 k = bytes32Pubkey ^ sha256(stringToBytes(jwt));
+    console.logBytes32(pendingVerification[pendingVerification.length - 1]);
+    console.log("looking up key");
+    console.log("key is pendingVerification[pendingVerification.length - 1]?", k == pendingVerification[pendingVerification.length - 1]);
+    console.logBytes32(k);
+    console.logBytes32(pendingVerification[pendingVerification.length - 1]);
+    console.logBytes32(k ^ pendingVerification[pendingVerification.length - 1]);
     JWTProof memory jp = jwtProofs[k];
+    // console.logBytes32(jp.hashedJWT);
+    console.logBytes32(k);
+    // console.log(keccak256(stringToBytes(jwt)));
+    console.log("JWT argument", jwt);
+    console.log("Block number", jp.blockNumber);
+    console.log("hashedJWT is: "); console.logBytes32(jp.hashedJWT);
     require(jp.blockNumber < block.number, "You need to prove knowledge of JWT in a previous block, otherwise you can be frontrun");
     require(jp.hashedJWT == keccak256(stringToBytes(jwt)), "JWT does not match JWT in proof");
     return true;
@@ -133,7 +168,19 @@ contract VerifyJWT {
     // check whether JWT is valid 
     require(verifyJWT(jwt),"Verification of JWT failed");
     // check whether sender has already proved knowledge of the jwt
-    require(_checkJWTProof(msg.sender, jwt), "Proof of previous knowlege of JWT unsuccessful");
+    require(checkJWTProof(msg.sender, jwt), "Proof of previous knowlege of JWT unsuccessful");
+  }
+
+  // kind of a hack; this view function is just for the frontend to call because it's easier to write code to XOR uint256s in Solidity than JS...idieally, this is done in browser
+  // It also allows your node provider to frontrun you, as you are trusting them with the JWT hash, but i don't think that will happen ;) still not decentralized enough, and should be put browser-side
+  // I don't think anyone else can frontrun you because I don't think view/pure functions are submitted to the mempool
+  function XOR(uint256 x, uint256 y) public pure returns (uint256) {
+    return x ^ y;
+  }
+  
+  // Testing function, remove later; this seems to give a different result than ethers.js sha256, perhaps because of byte conversion?
+  function testSHA256OnJWT(string memory jwt) public pure returns (bytes32){
+    return sha256(stringToBytes(jwt));
   }
   
 }
--- backend/test/verifyjwt.js
+++ backend/test/verifyjwt.js
@@ -1,10 +1,10 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("modExp works", function () {
-  it("Test modExp on some simple numbers", async function () {
+describe('modExp works', function () {
+  it('Test modExp on some simple numbers', async function () {
     const [owner] = await ethers.getSigners();
-    const vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy()
+    const vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy(11,230,56)
     let tx = await vjwt.verifyJWT(75,1,8001, 4)
     // 75^55 mod 8001 == 5984
     console.log('transaction', tx)
@@ -19,3 +19,74 @@ describe("modExp works", function () {
     
   });
 });
+
+describe('type conversion and cryptography', function (){
+  before(async function(){
+    [this.owner] = await ethers.getSigners();
+    this.vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy(11,230,56)
+    this.message = 'Hey'
+  })
+
+  it('sha256 hashing gives the same result on chain and frontend', async function () {
+    const publicHashedMessage = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(this.message))
+    const secretHashedMessage = ethers.utils.sha256(ethers.utils.toUtf8Bytes(this.message))  
+    expect(await this.vjwt.testSHA256OnJWT(this.message)).to.equal(secretHashedMessage)
+})
+it('XOR of pubkey and sha256(message) works as expected (integration test)', async function () {
+  const publicHashedMessage = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(this.message))
+  const XORedKey = await this.vjwt.XOR(
+    (await this.vjwt.testSHA256OnJWT(this.message)), 
+    (this.owner.address)
+  )
+  expect(XORedKey).to.equal(ethers.BigNumber.from('0x581d43745726e0ee6291116c4c2c5e99254f1d246a251ea78c3280d175c9b21c'))
+})
+})
+
+// 39855234793743226798606417 537894605540194011154359119936879820814189461090844, 
+// 39855234793743226798606417 537894605540194011154359119936879820814189461090844     
+// 39855234793743226798606417 555432625188164846172803337929475217050060651606556           
+describe('proof of prior knowledge', function () {
+  it('test with sample message (not JWT but can be)', async function () {
+    const [owner] = await ethers.getSigners();
+    const vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy(11,230,56)
+    const message = 'Hey'
+    // Must use two unique hashing algorithms
+    //  If not, hash(JWT) would be known, so then XOR(public key, hash(JWT)) can be replaced with XOR(frontrunner pubkey, hash(JWT)) by a frontrunner
+    const publicHashedMessage = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(message))
+    const secretHashedMessage = ethers.utils.sha256(ethers.utils.toUtf8Bytes(message))
+    
+    const proof = await vjwt.XOR(secretHashedMessage, owner.address)
+    await vjwt.commitJWTProof(proof, publicHashedMessage)
+    await ethers.provider.send('evm_mine')
+    expect(await vjwt.checkJWTProof(owner.address, message)).to.equal(true)
+    
+    
+    
+      // console.log(Buffer.from(secretHashedMessage, 'hex'), Buffer.from(owner.address, 'hex'))
+      // console.log(parseInt(secretHashedMessage, 16), parseInt(owner.address, 16))
+      // console.log(await vjwt.jwtProofs(proof));
+      
+  });
+});
+
+
+describe('ajfaksjnfalskjfd', function () {
+  it('asd;llf', async function () {
+    const [owner] = await ethers.getSigners();
+    const vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy(11,230,56)
+    await vjwt.testFunction(78)
+    await ethers.provider.send('evm_mine')
+    expect(await vjwt.e()).to.equal(78)
+    await vjwt.testFunction(70)
+    await ethers.provider.send('evm_mine')
+    expect(await vjwt.e()).to.equal(70)
+    
+    
+    
+      // console.log(Buffer.from(secretHashedMessage, 'hex'), Buffer.from(owner.address, 'hex'))
+      // console.log(parseInt(secretHashedMessage, 16), parseInt(owner.address, 16))
+      // console.log(await vjwt.jwtProofs(proof));
+      
+  });
+});
+

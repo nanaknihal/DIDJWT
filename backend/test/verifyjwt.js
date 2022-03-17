73bed0a9f9d399326cd4d68a190e49d249c4de8b
nanaknihal <nanaknihal@gmail.com>
2022-03-16 21:44:30 -0400

    commiting before switching branches and changing stuff

--- backend/test/verifyjwt.js
+++ backend/test/verifyjwt.js
@@ -277,8 +277,45 @@ describe('Integration tests for after successful proof commit', function () {
     expect(registeredAddresses[0] === this.owner.address).to.equal(true);
   });
 
+  
     // TODO: add tests for address => creds,  address => JWT,  JWT => address
 
 
 });
 
+describe('Anonymous proof commit', function () {
+  before(async function(){
+    [this.owner, this.addr1] = await ethers.getSigners()
+
+    const orig = 'access_token=117a16aa-f766-4079-ba50-faaf0a09c864&token_type=bearer&expires_in=599&tokenVersion=1&persistent=true&id_token=eyJraWQiOiJwcm9kdWN0aW9uLW9yY2lkLW9yZy03aGRtZHN3YXJvc2czZ2p1am84YWd3dGF6Z2twMW9qcyIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiX1RCT2VPZ2VZNzBPVnBHRWNDTi0zUSIsImF1ZCI6IkFQUC1NUExJMEZRUlVWRkVLTVlYIiwic3ViIjoiMDAwMC0wMDAyLTIzMDgtOTUxNyIsImF1dGhfdGltZSI6MTY0NDgzMDE5MSwiaXNzIjoiaHR0cHM6XC9cL29yY2lkLm9yZyIsImV4cCI6MTY0NDkxODUzNywiZ2l2ZW5fbmFtZSI6Ik5hbmFrIE5paGFsIiwiaWF0IjoxNjQ0ODMyMTM3LCJmYW1pbHlfbmFtZSI6IktoYWxzYSIsImp0aSI6IjcxM2RjMGZiLTMwZTAtNDM0Mi05ODFjLTNlYjJiMTRiODM0OCJ9.VXNSFbSJSdOiX7n-hWB6Vh30L1IkOLiNs2hBTuUDZ4oDB-cL6AJ8QjX7wj9Nj_lGcq1kjIfFLhowo8Jy_mzMGIFU8KTZvinSA-A-tJkXOUEvjUNjd0OfQJnVVJ63wvp9gSEj419HZ13Lc2ci9CRY7efQCYeelvQOQvpdrZsRLiQ_XndeDw2hDLAmI7YrYrLMy1zQY9rD4uAlBa56RVD7me6t47jEOOJJMAs3PC8UZ6pYyNc0zAjQ8Vapqz7gxeCN-iya91YI1AIE8Ut19hGgVRa9N7l-aUielPAlzss0Qbeyvl0KTRuZWnLUSrOz8y9oGxVBCUmStEOrVrAhmkMS8A&tokenId=254337461'
+    let parsedToJSON = {}
+    orig.split('&').map(x=>{let [key, value] = x.split('='); parsedToJSON[key] = value});
+    let [headerRaw, payloadRaw, signatureRaw] = parsedToJSON['id_token'].split('.');
+    // let [header, payload] = [headerRaw, payloadRaw].map(x => JSON.parse(atob(x)));
+    // let payload = atob(payloadRaw);
+    this.signature = Buffer.from(signatureRaw, 'base64url')
+    const pubkey = JSON.parse('{"keys":[{"kty":"RSA","e":"AQAB","use":"sig","kid":"production-orcid-org-7hdmdswarosg3gjujo8agwtazgkp1ojs","n":"jxTIntA7YvdfnYkLSN4wk__E2zf_wbb0SV_HLHFvh6a9ENVRD1_rHK0EijlBzikb-1rgDQihJETcgBLsMoZVQqGj8fDUUuxnVHsuGav_bf41PA7E_58HXKPrB2C0cON41f7K3o9TStKpVJOSXBrRWURmNQ64qnSSryn1nCxMzXpaw7VUo409ohybbvN6ngxVy4QR2NCC7Fr0QVdtapxD7zdlwx6lEwGemuqs_oG5oDtrRuRgeOHmRps2R6gG5oc-JqVMrVRv6F9h4ja3UgxCDBQjOVT1BFPWmMHnHCsVYLqbbXkZUfvP2sO1dJiYd_zrQhi-FtNth9qrLLv3gkgtwQ"}]}')
+    const [e, n] = [
+      ethers.BigNumber.from(Buffer.from(pubkey.keys[0]['e'], 'base64url')), 
+      Buffer.from(pubkey.keys[0]['n'], 'base64url')
+    ]
+
+    this.vjwt = await (await ethers.getContractFactory('VerifyJWT')).deploy(e,n, '0x222c22737562223a22', '0x222c22617574685f74696d65223a');
+    this.message = sha256FromString(headerRaw + '.' + payloadRaw)
+    this.payloadIdx = Buffer.from(headerRaw).length + 1 //Buffer.from('.').length == 1
+    this.sandwich = await sandwichIDWithBreadFromContract('0000-0002-2308-9517', this.vjwt);
+    this.wrongSandwich = await sandwichIDWithBreadFromContract('0200-0002-2308-9517', this.vjwt);
+    // find indices of sandwich in raw payload:
+    let [startIdx, endIdx] = search64.searchForPlainTextInBase64(Buffer.from(this.sandwich, 'hex').toString(), payloadRaw)
+    this.startIdx = startIdx; this.endIdx = endIdx
+    let publicHashedMessage = keccak256FromString(this.message)
+    let secretHashedMessage = sha256FromString(this.message)
+    let proof = await this.vjwt.XOR(secretHashedMessage, this.owner.address)
+
+    await this.vjwt.commitJWTProof(proof)
+    await ethers.provider.send('evm_mine')
+  });
+  it('jfakjfak', async function (){
+    console.log('a;jfak;sjfhaksjhdfa')
+  })
+});

631e43dfd545fb005e656c099977fc25c93a3f6f
Caleb <1calebtuttle@gmail.com>
2022-04-02 10:40:26 -0400

    tighten up idAggregator test code

--- backend/test/identityAggregator.js
+++ backend/test/identityAggregator.js
@@ -5,49 +5,19 @@ const { solidity } = require("ethereum-waffle");
 const search64 = require('../../../whoisthis.wtf-frontend/src/searchForPlaintextInBase64.js');
 // import { fixedBufferXOR as xor, sandwichIDWithBreadFromContract, padBase64, hexToString, searchForPlainTextInBase64 } from 'wtfprotocol-helpers';
 const { hexToString } = require('wtfprotocol-helpers');
-
+const {
+  orcidKid, orcidBotomBread, orcidTopBread,
+  deployVerifyJWTContract,
+  deployIdAggregator,
+  sha256FromString,
+  sandwichIDWithBreadFromContract,
+  jwksKeyToPubkey,
+} = require('./utils/utils');
 
 // chai.use(solidity);
 
 
-//-------------------- Constants & Helpers --------------------
-
-const orcidKid = '7hdmdswarosg3gjujo8agwtazgkp1ojs'
-const orcidBotomBread = '0x222c22737562223a22'
-const orcidTopBread = '0x222c22617574685f74696d65223a'
-
-const deployVerifyJWTContract = async (...args) => {
-  let VJWT = await ethers.getContractFactory('VerifyJWT')
-  return await upgrades.deployProxy(VJWT, args, {
-    initializer: 'initialize',
-  });
-}
-
-const sha256FromString = x => ethers.utils.sha256(ethers.utils.toUtf8Bytes(x))
-
-// Make sure it does bottomBread + id + topBread and does not allow any other text in between. If Google changes their JWT format so that the sandwich now contains other fields between bottomBread and topBread, this should fail until the contract is updated. 
-const sandwichIDWithBreadFromContract = async (id, contract) => {
-  let sandwich = (await contract.bottomBread()) + Buffer.from(id).toString('hex') + (await contract.topBread());
-  sandwich = sandwich.replaceAll('0x', '');
-  return sandwich
-}
-
-// Converts JWKS RSAkey to e and n:
-const jwksKeyToPubkey = (jwks) => {
-  let parsed = JSON.parse(jwks)
-  return [
-    ethers.BigNumber.from(Buffer.from(parsed['e'], 'base64url')), 
-    ethers.BigNumber.from(Buffer.from(parsed['n'], 'base64url'))
-  ]
-}
-
-//-------------------------------------------------
-
-
-
-describe.only("IdentityAggregator", function () {
-
-  // TODO: What's the best way to initialize contract variables before all tests, instead of initializing within every test?
+describe("IdentityAggregator", function () {
 
   describe("keywords", function () {
 
@@ -62,48 +32,32 @@ describe.only("IdentityAggregator", function () {
     });
     
     it("Should be updated when support for a new contract is added", async function () {
-      //--------------------------- Set up context ---------------------------
-      // get mock vjwt contract address
-      const [owner] = await ethers.getSigners()
-      const transactionCount = await owner.getTransactionCount()
-      const verifyJWTAddress = getContractAddress({
-        from: owner.address,
-        nonce: transactionCount
-      })
       const keyword = 'orcid';
-      await this.idAggregator.addPlatformContract(keyword, verifyJWTAddress);
-
-      //--------------------------- Run test ---------------------------
-
+      await this.idAggregator.addPlatformContract(keyword, "0xABCDEF1234567890ABCDEF1234567890ABCDEF12");
       expect(await this.idAggregator.getKeywords()).to.have.members([keyword]);
     });
   });
 
   describe("contractAddrForKeyword", function () {
+    
     it("Should be updated when support for a new contract is added", async function () {
-      //--------------------------- Set up context ---------------------------
-
-      const IdentityAggregator = await ethers.getContractFactory("IdentityAggregator");
-      const idAggregator = await IdentityAggregator.deploy();
-      await idAggregator.deployed();
+      const idAggregator = await deployIdAggregator();
+      const [owner] = await ethers.getSigners();
 
       // get contract address for VerifyJWT
-      const [owner] = await ethers.getSigners()
-      const transactionCount = await owner.getTransactionCount()
-      const futureAddress = getContractAddress({
+      const transactionCount = await owner.getTransactionCount();
+      const vjwtAddress = getContractAddress({
         from: owner.address,
         nonce: transactionCount
-      })
+      });
 
       vjwt = await deployVerifyJWTContract(11, 59, orcidKid, orcidBotomBread, orcidTopBread);
 
       const keyword = "orcid";
-      await idAggregator.addPlatformContract(keyword, futureAddress);
-
-      //--------------------------- Run test ---------------------------
+      await idAggregator.addPlatformContract(keyword, vjwtAddress);
 
       const verifyJWTAddress = await idAggregator.callStatic.contractAddrForKeyword(keyword);
-      expect(verifyJWTAddress).to.equal(futureAddress);
+      expect(verifyJWTAddress).to.equal(vjwtAddress);
     });
   });
 
@@ -111,17 +65,15 @@ describe.only("IdentityAggregator", function () {
     it("Should return array of supported creds", async function() {
       //--------------------------- Set up context ---------------------------
 
-      const IdentityAggregator = await ethers.getContractFactory("IdentityAggregator");
-      const idAggregator = await IdentityAggregator.deploy();
-      await idAggregator.deployed();
+      const idAggregator = await deployIdAggregator();
+      const [owner] = await ethers.getSigners();
 
       // get contract address for VerifyJWT
-      const [owner] = await ethers.getSigners()
-      const transactionCount = await owner.getTransactionCount()
+      const transactionCount = await owner.getTransactionCount();
       const vjwtAddress = getContractAddress({
         from: owner.address,
         nonce: transactionCount
-      })
+      });
 
       const [eOrcid, nOrcid] = jwksKeyToPubkey('{"kty":"RSA","e":"AQAB","use":"sig","kid":"production-orcid-org-7hdmdswarosg3gjujo8agwtazgkp1ojs","n":"jxTIntA7YvdfnYkLSN4wk__E2zf_wbb0SV_HLHFvh6a9ENVRD1_rHK0EijlBzikb-1rgDQihJETcgBLsMoZVQqGj8fDUUuxnVHsuGav_bf41PA7E_58HXKPrB2C0cON41f7K3o9TStKpVJOSXBrRWURmNQ64qnSSryn1nCxMzXpaw7VUo409ohybbvN6ngxVy4QR2NCC7Fr0QVdtapxD7zdlwx6lEwGemuqs_oG5oDtrRuRgeOHmRps2R6gG5oc-JqVMrVRv6F9h4ja3UgxCDBQjOVT1BFPWmMHnHCsVYLqbbXkZUfvP2sO1dJiYd_zrQhi-FtNth9qrLLv3gkgtwQ"}');
       const vjwt = await deployVerifyJWTContract(eOrcid, nOrcid, orcidKid, orcidBotomBread, orcidTopBread);
